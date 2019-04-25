705445dcc16fe36189a785d9af95fd2e40100279
fortunate-jest-6040 <pedro.loureiro7@hotmail.com>
2019-04-25 20:10:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var katzDeliLine = [];
 
 function takeAnumber(Array,name)
+{
+  var index = 0;
+  Array.push(name);
+  index = Array.length;
+  
+  return "Welcome, " + name + ". You are number " + index + 
+}
