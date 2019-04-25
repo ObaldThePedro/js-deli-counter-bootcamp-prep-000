ac659e600479e6bcb36641a5cb5548dbf6405ca5
fortunate-jest-6040 <pedro.loureiro7@hotmail.com>
2019-04-25 20:13:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,16 @@ function takeAnumber(Array,name)
   return "Welcome, " + name + ". You are number " + index + " in line."
 }
 
+function nowServing(Array)
+{
+  if(Array.length === 0)
+  {
+    return "There is nobody to be served!";
+  }
+  else{
+    
+  }
+}
+
 console.log(takeAnumber(katzDeliLine,"Helen"));
 console.log(takeAnumber(katzDeliLine,"Pedro"));
